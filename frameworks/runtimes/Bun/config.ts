ec3c1948eb5b38f474b28a2e1b50cf0b4c6932a4
aquaplmc <aquaplmc@gmail.com>
2024-06-18 16:29:59 +0000

    Update

--- frameworks/runtimes/Bun/config.ts
+++ frameworks/runtimes/Bun/config.ts
@@ -1,7 +1,13 @@
 import type { Env, RuntimeConfig } from '@typings/runtime';
 import { $ } from 'bun';
 
-const env: Env = { NODE_ENV: 'production' };
+const env: Env = { 
+    NODE_ENV: 'production',
+    
+    BUN_JSC_jitPolicyScale: '0.0',
+    BUN_JSC_thresholdForOptimizeSoon: '0.0',
+    BUN_JSC_thresholdForJITSoon: '0.0',
+};
 
 const config: RuntimeConfig = {
     run: (path, cwd) => Bun.spawn(['bun', 'run', path], { env, cwd }),
